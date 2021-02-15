5c293090b2c5040f1c7554a93c0a5c1bf2d8fb55
IgorOlenchuk <67928928+IgorOlenchuk@users.noreply.github.com>
2021-02-15 23:45:57 +0300

    Add files via upload

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -140,17 +140,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 
 REST_FRAMEWORK = {
-        'DEFAULT_FILTER_BACKENDS': [
-            'django_filters.rest_framework.DjangoFilterBackend'
-        ],
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 10,
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-        ],
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework_simplejwt.authentication.JWTAuthentication',
-        ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'],
 }
 
 SIMPLE_JWT = {
